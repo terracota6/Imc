34b0b0f2bcab9bcabf94d545d26b080aa8152f2c
Vinicius Fagner da Silva <133381911+terracota6@users.noreply.github.com>
2024-08-30 14:49:50 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const nome = "Vinicus";
+const sobrenome = "Fagner";
+const idade = 16;
+const altura = 1.71;
+const massa = 53;
+
+const 
+
+console.log(`Paciente ${nome} ${sobrenome} tem idade ${idade} altura ${altura} massa ${massa} e seu imc é`)
